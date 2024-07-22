f7d7602b76a052df02d27bd5cb139b7762064874
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-22 17:05:36 +0200

    adjust config for custom URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app-template/',
+  // base: '/app-template/',
   plugins: [vue()],
   resolve: {
     alias: {
